3a34125adcfc6ddf4748a1a2730ccdefd976e93d
AzureBlue <charlie800228@gmail.com>
2021-12-17 00:31:57 +0800

    feat: renew 2021 files

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <el-container>
       <el-header style="height: auto;">
-        <h1>2020 臺北醫學大學藥學系<br>系友回娘家</h1>
+        <h1>2021 臺北醫學大學藥學系<br>系友回娘家</h1>
         <img class="banner" src="./assets/banner.jpg" alt="banner" />
       </el-header>
       <el-main>
@@ -44,8 +44,16 @@
           style="font-size: 1rem;"
         >
           <el-table-column
-            prop="graduate"
+            prop="grade"
             label="屆別">
+          </el-table-column>
+          <el-table-column
+            prop="year"
+            label="入學年">
+          </el-table-column>
+          <el-table-column
+            prop="number"
+            label="學籍編號">
           </el-table-column>
           <el-table-column
             prop="name"
@@ -62,53 +70,33 @@
 </template>
 
 <script>
-import getData from './api/api';
+import _ from 'lodash';
+import json from './assets/data.json';
 
 export default {
   name: 'app',
   data() {
     return {
-      rawData: [],
-      filterData: [],
-      tableData: [],
+      rawData: json,
+      tableData: json,
       filterText: '',
       isLoading: false,
     };
   },
   methods: {
-    dataTransform(data) {
-      this.tableData = [];
-      data.forEach((item) => {
-        this.tableData.push({
-          graduate: item.gsx$graduate.$t,
-          name: item.gsx$name.$t,
-          table: item.gsx$table.$t,
-        });
-      });
-    },
-    async getFilterData() {
-      if (!this.filterText.length) return;
-      this.isLoading = true;
-      const data = await getData(this.filterText);
-      this.isLoading = false;
-
-      this.filterData = data.feed.entry;
-      this.dataTransform(this.filterData);
-    },
     resetData() {
       this.isLoading = true;
       this.filterText = '';
-      this.dataTransform(this.rawData);
+      this.tableData = this.rawData;
       this.isLoading = false;
     },
-  },
-  created() {
-    this.isLoading = true;
-    getData().then((data) => {
-      this.rawData = data.feed.entry;
-      this.dataTransform(this.rawData);
-      this.isLoading = false;
-    });
+    getFilterData() {
+      this.tableData = this.rawData.filter((row) => {
+        const rowValues = _.values(row);
+
+        return rowValues.some(text => text.search(this.filterText) >= 0);
+      });
+    },
   },
 };
 </script>
